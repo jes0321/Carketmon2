4b11b73e94bb35daeed1117301d42e96c0fe608d
jes0321 <jesjes0321@naver.com>
2025-11-25 14:46:51 +0900

    fix : UnitObject

--- 2025_winapi_framework_23/UnitObject.h
+++ 2025_winapi_framework_23/UnitObject.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Object.h"
-class UnitData;
+#include "UnitData.h"
 class UnitObject : public Object
 {
 public:
@@ -12,24 +12,6 @@ public:
 	UnitData* GetUnitData() const { return m_unitData; }
 	CardData* GetCardRandom() const { return m_unitData->GetCardRandom(); }
 private :
-	int m_currentHp;
-	UnitData* m_unitData;
-};
-
-#pragma once
-#include "Object.h"
-#include"UnitData.h"
-class UnitObject : public Object
-{
-public:
-	void Render(HDC _hdc) override;
-public:
-	void SetUnitData(UnitData* _data);
-	void UseCard(int index) { m_unitData->UseCard(index); }
-	void Damage(int dmg) { m_currentHp -= dmg; if (m_currentHp < 0) m_currentHp = 0; }
-	UnitData* GetUnitData() const { return m_unitData; }
-	CardData* GetCardRandom() const { return m_unitData->GetCardRandom(); }
-private:
 	int m_currentHp;
 	UnitData* m_unitData;
 };
